edbc31d11f6605edaa2bbfd1fadf1cf602e18117
Steven Kim <stevenkim1989@gmail.com>
2020-03-26 17:21:40 -0700

    add login page and firebase api.js in scripts folder

---
+++ scripts/firebase_api_StudyFarm.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAd1XdKFjA6lCiOqQngcl0_1EIGavgX3fQ",
+  authDomain: "studyfarm-52040.firebaseapp.com",
+  databaseURL: "https://studyfarm-52040.firebaseio.com",
+  projectId: "studyfarm-52040",
+  storageBucket: "studyfarm-52040.appspot.com",
+  messagingSenderId: "608975569339",
+  appId: "1:608975569339:web:b9396b5b2dc84aa293c49e",
+  measurementId: "G-7LRJ62FG7Z"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
