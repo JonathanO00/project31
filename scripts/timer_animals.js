89ffdbdff7b767494fb3454e9f438287a3bda343
bdiu <bdiu23@gmail.com>
2020-03-31 03:41:08 -0700

    fixed the timer to automatically start the 5 min break timer

--- scripts/timer_animals.js
+++ scripts/timer_animals.js
@@ -1,6 +1,7 @@
 const FULL_DASH_ARRAY = 283;
-const WARNING_THRESHOLD = 10;
-const ALERT_THRESHOLD = 5;
+const WARNING_THRESHOLD = 120;
+const ALERT_THRESHOLD = 60;
+
 
 const COLOR_CODES = {
   info: {
@@ -16,9 +17,13 @@ const COLOR_CODES = {
   }
 };
 
-const TIME_LIMIT = 1200;
+const TIME_LIMIT = 1200; // 20 min
+const BREAK_LIMIT = 300; // 5 min
+const TIMER_MS = 1201100; // 20 min 1.1 second
+const BREAK_MS = 301000; // 5 min 1 second
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
+let breakLeft = BREAK_LIMIT;
 let timerInterval = null;
 let remainingPathColor = COLOR_CODES.info.color;
 
@@ -46,6 +51,10 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
+// this redirects the page to the endtimer page where a 5 
+// min break timer begins. 
+setTimeout(startBreak, TIMER_MS); //This one should be good.
+
 startTimer();
 
 function onTimesUp() {
@@ -56,14 +65,28 @@ function startTimer() {
   timerInterval = setInterval(() => {
     timePassed = timePassed += 1;
     timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById("base-timer-label").innerHTML = formatTime(
-      timeLeft
-    );
+    document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
     setCircleDasharray();
     setRemainingPathColor(timeLeft);
 
     if (timeLeft === 0) {
       onTimesUp();
+    }
+  }, 1000);
+}
+
+function startBreak() {
+  timePassed = 0; // this one line of code took me 1 hour to figure out ..
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    breakLeft = BREAK_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(breakLeft);
+    setCircleDasharray2();
+    //setRemainingPathColor(breakLeft); // This line may be redundant
+
+    if (breakLeft === 0) {
+      onTimesUp();
+      //add funtion here that will create a button that will restart the timer once its clicked.
     }
   }, 1000);
 }
@@ -106,6 +129,20 @@ function calculateTimeFraction() {
 function setCircleDasharray() {
   const circleDasharray = `${(
     calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+function calculateTimeFraction2() {
+  const rawTimeFraction = breakLeft / BREAK_LIMIT;
+  return rawTimeFraction - (1 / BREAK_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray2() {
+  const circleDasharray = `${(
+    calculateTimeFraction2() * FULL_DASH_ARRAY
   ).toFixed(0)} 283`;
   document
     .getElementById("base-timer-path-remaining")
