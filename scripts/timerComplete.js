732976dcdb5298f826d84bdd645622a2c4282684
Jonathan Orfani <jonathanorfani@gmail.com>
2020-03-31 19:01:20 -0700

    added some other stuff, edited unfinished pages

--- scripts/timerComplete.js
+++ scripts/timerComplete.js
@@ -1,10 +1,15 @@
 function addToFarm() {
     let animalName = document.getElementById("animalName").value;
 
-    // Add a new document in collection "animals"
-    db.collection("users").doc(user.uid).set({ //How do you refer to a user's auto-generated ID?????
-            animal: pig,
-            name: animalName
+    let animalObj = {
+        animal: "pig",
+        name: animalName
+    }
+
+    firebase.auth().onAuthStateChanged(function (user) {
+            db.collection("users").doc(user.uid)
+                .collection("animals")
+                .add(animalobj); //ugly ID auto generated for the animal
         })
         .then(function () {
             console.log("Document successfully written!");
@@ -13,3 +18,6 @@ function addToFarm() {
             console.error("Error writing document: ", error);
         });
 }
+
+//document.getElementById("addToFarm").onclick = addToFarm;
+document.getElementById("addToFarm").onclick = "location.href='myfarm.html'";
